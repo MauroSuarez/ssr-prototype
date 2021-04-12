91c97a401ea1fe090f6c032ccc7fef55cf7dc5de
Mauro Suarez <msuarez@MacBook-Pro-de-Administrador.local>
2021-04-12 11:35:38 -0300

    Add tailwindcss and config

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+//import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="mt-4 p-4 w-1/4 rounded bg-blue-300 text-center">
+        <p className="text-blue-600">This should be very blue.</p>
+      </div>
+    </div>
+  )
+}
